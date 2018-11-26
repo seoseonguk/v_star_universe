57eeca3974151d8e9ccb788d6cfcc80eef7230d5
seonguk-mac <su.seo@bakecompany.net>
2018-11-26 13:37:05 +0900

    [add] allowed host url 추가

--- v_star_universe/settings/dev.py
+++ v_star_universe/settings/dev.py
@@ -1,5 +1,5 @@
 from .common import *
 
-# INSTALLED_APPS += ['debug_toolbar']
+INSTALLED_APPS += ['debug_toolbar']
 MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
 INTERAL_IPS = ['127.0.0.1']
