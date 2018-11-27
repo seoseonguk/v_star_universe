af7a2d951a03de51246f9fbcdddc52f06c689280
seonguk-mac <su.seo@bakecompany.net>
2018-11-27 21:41:49 +0900

    [add]add S3 settings

--- v_star_universe/settings/prod.py
+++ v_star_universe/settings/prod.py
@@ -3,36 +3,7 @@ from .common import *
 DEBUG = False
 
 
-
-# import os
-# import pymysql
-# pymysql.install_as_MySQLdb()
-
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.mysql'),
-#         'HOST': os.environ['DB_HOST'],
-#         'USER': os.environ['DB_USER'],
-#         'PASSWORD': os.environ['DB_PASSWORD'],
-#         'NAME': os.environ['DB_NAME'],
-#         'PORT': os.environ['DB_PORT'],
-#     }
-# }
-
 INSTALLED_APPS += ['sentry_sdk',]
-
-# STATICFILES_STORAGE = 'v_star_universe.storages.StaticS3Boto3Storage'
-# DEFAULT_FILE_STORAGE = 'v_star_universe.storages.MediaS3Boto3Storage'
-
-# AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-# AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-# AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME'] # 필수 지정
-# AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'ap-northeast-2')
-#
-#
-
 import sentry_sdk
 
 from sentry_sdk.integrations.django import DjangoIntegration
@@ -44,4 +15,29 @@ sentry_sdk.init(
     integrations=[DjangoIntegration()]
 
 )
+
+import os
+import pymysql
+pymysql.install_as_MySQLdb()
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.mysql'),
+        'HOST': os.environ['DB_HOST'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'NAME': os.environ['DB_NAME'],
+        'PORT': os.environ['DB_PORT'],
+    }
+}
+
+INSTALLED_APPS += ['storages']
+STATICFILES_STORAGE = 'v_star_universe.storages.StaticS3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'v_star_universe.storages.MediaS3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME'] # 필수 지정
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'ap-northeast-2')
+
 
---
+++ v_star_universe/storages.py
@@ -0,0 +1,7 @@
+from storages.backends.s3boto3 import S3Boto3Storage
+
+class StaticS3Boto3Storage(S3Boto3Storage):
+    location = 'static'
+
+class MediaS3Boto3Storage(S3Boto3Storage):
+    location = 'media'
