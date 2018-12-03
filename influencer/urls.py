5a608531b5572c659216129f63ee1a93f097c865
seonguk-mac <su.seo@bakecompany.net>
2018-12-04 03:41:39 +0900

    [edit] url redirect 수정

--- influencer/urls.py
+++ influencer/urls.py
@@ -4,9 +4,7 @@ from django.views.generic import TemplateView
 from .views import InfluencerListView
 
 urlpatterns = [
-    path('influencer/', TemplateView.as_view(template_name="influencer/index.html")),
-    path('signup/', TemplateView.as_view(template_name="influencer/signup.html")),
-
+    path('', TemplateView.as_view(template_name="influencer/index.html")),
     path('influencer/list', InfluencerListView.as_view())
 
 ]
